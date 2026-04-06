3bd6f5539f136c7ef73f672da47a780bc2448af5
Baden Joseph (RIT Student) <bcj9505@rit.edu>
2026-04-06 10:47:30 -0400

    Q1 Completed

--- src/main/java/mte3/tempconvert/Converter.java
+++ src/main/java/mte3/tempconvert/Converter.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class Converter {
     
     private static class CelsiusToFahrenheit implements TempConvert {
-
         @Override
         public double convert(double temp) {    return temp * 9.0 / 5.0 + 32;    }
         
@@ -17,17 +16,20 @@ public class Converter {
         
         try(Scanner scanner = new Scanner(System.in)) {
             System.out.print("Please enter temperature (for conversion): ");
-
+            Double in = scanner.nextDouble();
             // (part 1) conversion from C to F
-            
-            
+            System.out.println("C to F: " + new CelsiusToFahrenheit().convert(in));
             // (part 2) conversion from F to C
-            
-            
+            TempConvert f2c = new TempConvert() {
+                @Override
+                public double convert(double temp) {
+                    return (temp - 32) * 5 / 9;
+                }
+            };
+            System.out.println("F to C: " + f2c.convert(in));
             // (part 3) conversion from F to K
-            
-
-
+            TempConvert f2k = (a) -> (a - 32) * 5 / 9 + 273.15;
+            System.out.println("F to K: " + f2k.convert(in));
         }  // try { } block closed
 
     } // main () method closed
